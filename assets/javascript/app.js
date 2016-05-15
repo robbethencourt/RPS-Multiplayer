15ed3e327426802aaccd7f44884f0dfe5c0c74c8
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-05-15 18:11:16 -0400

    turns logic updating on turns value update in firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,6 +50,7 @@ $(document).ready(function(){
 								choice: choice
 							}
 						}, 
+							// set the turns object to 0
 							turns: turns
 					}); // end set
 
@@ -59,14 +60,16 @@ $(document).ready(function(){
 				// if there is data in firebase
 				} else {
 
+					// increment the turns by 1
 					turns++;
+
+					// update the turns object in firebase
+					dataRef.update({
+						turns: turns
+					}); //  end firebase update
 
 					// store teh players child as a variable
 					var child_to_update = dataRef.child('players');
-
-					dataRef.update({
-						turns: turns
-					})
 
 					// update the players object with the 2 player with what the user entered	
 					child_to_update.update({
@@ -101,11 +104,14 @@ $(document).ready(function(){
 
 
 		// updating the screen with firebase data
-		dataRef.on('child_changed', function(snapshot) {
+		dataRef.on('value', function(snapshot) {
 
 			// store each player into it's own variable
-			var player1 = snapshot.val().player1;
-			var player2 = snapshot.val().player2;
+			var player1 = snapshot.val().players.player1;
+			var player2 = snapshot.val().players.player2;
+
+			var current_turn = snapshot.val().turns;
+			console.log(current_turn);
 
 			// if the palyer number is set to 1
 			if (player_number === 1) {
@@ -116,10 +122,37 @@ $(document).ready(function(){
 				$('#opponent-name-score').html(player2.name); // opponents name
 				$('#opponent-wins').html(player2.wins); // oponents wins
 				$('#opponent-losses').html(player2.losses); // opponents losses
-				$('#game-message').html("it's your turn so choose wiesely");
 
-			// if the player number is other than 1
-			} else {
+				// if it's player1's turn
+				if (current_turn === 1) {
+
+					// remove the not-visible class
+					$('.rps-buttons').removeClass('not-visible');
+					// update game message
+					$('#game-message').html("it's your turn so choose wiesely");
+
+				} // end if
+
+				// if it's player2's turn
+				if (current_turn === 2) {
+
+					// add the not-visible class
+					$('.rps-buttons').addClass('not-visible');
+				
+				} // end if
+
+				// if it's nobody's turn
+				if (current_turn === 0) {
+
+					// add the not-vsible class
+					$('.rps-buttons').addClass('not-visible');
+
+				} // end if
+
+			} // end if
+
+			// if the player number is not 1
+			if (player_number !== 1) {
 
 				// update the screen with the below items
 				$('#player-wins').html(player2.wins); // player wins
@@ -128,10 +161,34 @@ $(document).ready(function(){
 				$('#opponent-wins').html(player1.wins); // opponent wins
 				$('#opponent-losses').html(player1.losses); // opponent losses
 
-			} // end if else
+				// if it's player1's turn
+				if (current_turn === 2) {
 
-			// removing the not-visible class when both users are in firebase
-			$('.rps-buttons').removeClass('not-visible');
+					// remove the not-visible class
+					$('.rps-buttons').removeClass('not-visible');
+
+				} // end if
+
+				// if it's player2's turn
+				if (current_turn === 1) {
+
+					// add the not-visible class
+					$('.rps-buttons').addClass('not-visible');
+					// update game message
+					$('#game-message').html("it's " + player1.name + "'s turn so wiat 'til they decide");
+				
+				} // end if
+
+				// if it's nobody's turn
+				if (current_turn === 0) {
+
+					// add the not-vsible class
+					$('.rps-buttons').addClass('not-visible');
+
+				} // end if
+
+
+			} // end if
 
 		}); // end updating screen
 
